7338e5e9c6a5e0a399a503c3cb4ccd814bfcf877
Alassane-diaa <dialassane19@gmail.com>
2026-01-31 00:07:04 +0100

    Fetching all home data at once

--- src/Fetcher.jsx
+++ src/Fetcher.jsx
@@ -3,28 +3,29 @@ import { useState } from "react";
 const TMDB_BASE_URL = import.meta.env.VITE_TMDB_BASE_URL;
 const TMDB_TOKEN = import.meta.env.VITE_TMDB_TOKEN;
 
-export const fetchTrending = async () => {
+export const fetchHomeData = async () => {
   const options = {
     method: 'GET',
     headers: {
       accept: 'application/json',
-      Authorization: `Bearer ${TMDB_TOKEN}`
+      Authorization: `Bearer ${import.meta.env.VITE_TMDB_TOKEN}`
     }
   };
 
   try {
-    const response = await fetch(`${TMDB_BASE_URL}/trending/movie/week?language=fr-FR`, options);
-    
-    if (!response.ok) {
-      throw new Error(`Erreur HTTP: ${response.status}`);
-    }
+    const [trending, topRated, upcoming] = await Promise.all([
+      fetch(`${import.meta.env.VITE_TMDB_BASE_URL}/trending/movie/week?language=fr-FR`, options).then(res => res.json()),
+      fetch(`${import.meta.env.VITE_TMDB_BASE_URL}/movie/top_rated?language=fr-FR`, options).then(res => res.json()),
+      fetch(`${import.meta.env.VITE_TMDB_BASE_URL}/movie/upcoming?language=fr-FR`, options).then(res => res.json()),
+    ]);
 
-    const data = await response.json();
-    console.log("Données reçues:", data);
-    return data.results;
+    return {
+      trending: trending.results,
+      topRated: topRated.results,
+      upcoming: upcoming.results
+    };
   } catch (error) {
-    console.error("Erreur lors du fetch trending:", error);
-    return []; 
+    console.error("Erreur Home Data:", error);
+    return null;
   }
-
 };
